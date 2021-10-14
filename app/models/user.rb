6b70768ed28663f8b62e9b524a1ef25c5c70b9b5
mudl <codybaldwin@gmail.com>
2021-10-14 21:47:15 +0000

    Devise and SimpleDiscussion

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  include SimpleDiscussion::ForumUser
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+end
