a30cdb8b2b09ca215f8c807c43ea17d094c51737
mudl <codybaldwin@gmail.com>
2021-10-15 03:05:13 +0000

    Still trying to fix Bootstrap in production, working in dev

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,21 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default'],
-    action_cable: 'action_cable',
-  }),
-  new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
-)
-
-const config = environment.toWebpackConfig();
-
-config.resolve.alias = {
-  jquery: 'jquery/src/jquery'
-};
-
 module.exports = environment
