d91c0c9650faf0d0f2a4d845610509dd10695eee
mudl <codybaldwin@gmail.com>
2021-10-24 09:15:53 +0000

    Mailgun in production through Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # See: https://devcenter.heroku.com/articles/mailgun
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'the-flying-dutchmen.herokuapp.com/',
+    :authentication => :plain,
+  }
+  config.action_mailer.delivery_method = :smtp
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
