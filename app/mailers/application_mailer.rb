8d58c12b968edf258589f33cef9ea1b589b0f250
mudl <codybaldwin@gmail.com>
2021-10-25 06:39:10 +0000

    change default from: address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'notifications@theflyingdutchmen.net'
   layout 'mailer'
+
 end
 
 # For simple tests.
