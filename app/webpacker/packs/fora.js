2e10e0b959b8b628600c3ad8c6e08b111831ea77
mudl <codybaldwin@gmail.com>
2021-10-29 04:45:07 +0000

    Seperating fora js from inline, moved to it's own section so webpacker and map it seperately from other stuff in the future.

---
+++ app/webpacker/packs/fora.js
@@ -0,0 +1,9 @@
+function scrollDown(){
+  window.scrollTo(0,document.body.scrollHeight);
+}
+scrollDown();
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
