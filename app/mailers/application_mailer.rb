db3a918e660d05b80dbbc181d99c795ed1c930c4
cmbaldwin <codybaldwin@gmail.com>
2024-06-12 16:56:47 +0900

    Attempt to fix the error with email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,15 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV["FROM_ADDRESS"]
+  default from: ENV.fetch('FLYINGD_SMTP_USER', nil)
   layout 'mailer'
-
 end
 
 # For simple tests.
 # ActionMailer::Base.mail(
-#   from: "test@example.co", 
-#   to: "valid.recipient@domain.com", 
-#   subject: "Test", 
+#   from: "test@example.co",
+#   to: "valid.recipient@domain.com",
+#   subject: "Test",
 #   body: "Test"
 # ).deliver_now
-# 
+#
 # Read more here: http://guides.rubyonrails.org/action_mailer_basics.html
